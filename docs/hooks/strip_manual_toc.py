5171f7df1f79bd1478040e313516981a3ac03ca6
Martin Blais <blais@furius.ca>
2026-04-11 18:59:47 -0400

    Fix image paths for use_directory_urls by stripping redundant page directory

--- docs/hooks/strip_manual_toc.py
+++ docs/hooks/strip_manual_toc.py
@@ -1,12 +1,13 @@
 import re
+import os
 
-def on_page_markdown(markdown, **kwargs):
+def on_page_markdown(markdown, page, **kwargs):
+    # 1. Strip manual TOCs
     lines = markdown.split('\n')
     new_lines = []
     in_toc_area = True
     
     # Pattern for manual TOC links: [<u>...</u>](#...) or > [<u>...</u>](#...)
-    # Also handles cases without <u> just in case.
     toc_pattern = re.compile(r'^(\s*>\s*)?\[(?:<u>)?.*(?:</u>)?\]\(#.*\)\s*$')
     
     for line in lines:
@@ -22,7 +23,19 @@ def on_page_markdown(markdown, **kwargs):
         
         new_lines.append(line)
         
-    # Remove excessive leading empty lines that might have been left behind
-    content = '\n'.join(new_lines)
-    # But keep the title!
-    return content
+    markdown = '\n'.join(new_lines)
+
+    # 2. Fix image paths for use_directory_urls: true
+    # If page is 'foo.md', and it contains <img src="foo/media/bar.png">,
+    # it should be converted to <img src="media/bar.png"> because with 
+    # use_directory_urls: true, the page is served from 'foo/index.html'
+    
+    page_name = os.path.splitext(os.path.basename(page.file.src_path))[0]
+    
+    # Replace src="page_name/media/ with src="media/
+    # Using 'media/' directly because the page is at 'page_name/' and the folder is at 'page_name/media/'
+    markdown = re.sub(fr'src="{page_name}/media/', 'src="media/', markdown)
+    # Also handle markdown-style images ![alt](page_name/media/...)
+    markdown = re.sub(fr'\]\({page_name}/media/', '](media/', markdown)
+
+    return markdown
